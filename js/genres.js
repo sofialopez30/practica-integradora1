8e5a679517d773d9e44e7c03c6a865f2305329fc
Victoria Moises <victoriamoises@MBP-de-Victoria.fibertel.com.ar>
2022-06-16 15:43:05 -0300

    Âsubo cambniis

---
+++ js/genres.js
@@ -0,0 +1,28 @@
+let querystring =location.storage;
+let querystringObt = new URLSearchParams(querystring);
+let idUurl =querystringObt.get("id");
+let urlapi = "https://api.allorigins.win/raw?url=https://api.deezer.com/genre" 
+
+fetch(urlapi)
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data);
+    let info = data.data;
+    let lista = document.querySelector('.generos')
+    let elementosLista = ''
+
+
+    for (let i=0; i<info.length; i++){
+        elementosLista += `<article class="genres">
+        <p><a href="detail-genres.html?id=${info[i].id}">${info[i].name}</a></p>
+        <img class="fotosgeneros" src= "${info[i].picture}" alt="">
+        </article>`
+    }
+    console.log(elementosLista);
+    lista.innerHTML += elementosLista;
+})
+.catch(function(error){
+    console.log(error)
+})
